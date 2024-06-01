ca855ba5207001f112ea409c9d021ea459f9e0fc
PabloGilberti <pgilberti@gmail.com>
2024-06-01 19:42:16 -0300

    Subienmdo archivois en rama1

---
+++ agregarUsuario.php
@@ -0,0 +1,17 @@
+<?php
+function agregarUsuario( $usuarios, $nombre,$edad, $email){
+
+                $usuarios=[
+                    "nombre"    =>$nombre,
+                    "edad"      =>$edad,
+                    "email"     =>$email
+                ];
+                return $usuarios ;                  
+}
+
+
+
+
+
+
+?>
